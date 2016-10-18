960a7300d32c7597f3751781308ff69e6a80f3f9
Vijayaraj <vijayaraj@kefassoftwares.com>
2016-10-18 18:58:37 +0530

    Compiled database files

--- src/Santiago/ErrorCategory.cpp
+++ src/Santiago/ErrorCategory.cpp
@@ -7,7 +7,7 @@ namespace Santiago
     std::error_condition ErrorCategory::default_error_condition(int code_) const noexcept
     {
         BOOST_ASSERT(_errorValueDescriptionMap.find(code_) != _errorValueDescriptionMap.end());
-        return std::error_condition(code_,GetInstance());
+        return std::error_condition(code_, GetInstance());
     }
 
     bool ErrorCategory::equivalent(int code_, const std::error_condition& condition_) const noexcept
@@ -43,7 +43,7 @@ namespace Santiago
              {ERR_USERNAME_ALREADY_EXISTS,         "Username already exists."},
              {ERR_INVALID_USERNAME_PASSWORD,       "Invalid username-password."},
              {ERR_INVALID_SESSION_COOKIE,          "Invalid session cookie."},
-             {ERR_INVALID_FASTCGI_REQUEST,         "Invalud fastcgi request."},
+             {ERR_INVALID_FASTCGI_REQUEST,         "Invalid fastcgi request."},
              {ERR_FASTCGI_REQUEST_ALREADY_REPLIED, "Fastcgi request already replied"}
          }
     {}
--- src/Santiago/SantiagoDBTables/MariaDBConnectionV1.cpp
+++ src/Santiago/SantiagoDBTables/MariaDBConnectionV1.cpp
@@ -1,7 +1,7 @@
-#include "../Utils/PTimeUtils.h"
-#include "../Utils/STLog.h"
+#include "Utils/PTimeUtils.h"
+#include "Utils/STLog.h"
 
-#include "MariaDBConnection.h"
+#include "MariaDBConnectionV1.h"
 
 namespace Santiago{ namespace SantiagoDBTables
 {
@@ -9,13 +9,13 @@ namespace Santiago{ namespace SantiagoDBTables
     MariaDBConnection::MariaDBConnection(const boost::property_tree::ptree& config_):
         _config(config_)
     {
-        std::error_code& error;
+        std::error_code error;
         connect(error);
     }
 
     MariaDBConnection::~MariaDBConnection()
     {
-        std::error_code& error;
+        std::error_code error;
         disconnect(error);
     }
 
@@ -25,58 +25,62 @@ namespace Santiago{ namespace SantiagoDBTables
 
         BOOST_ASSERT(_mysql != NULL); //This should be very rare. hence BOOST_ASSERT.
 
-        mysql_optionsv(mysql, MYSQL_OPT_RECONNECT, (void *)"1"); //set auto reconnect.
-                
+        mysql_options(_mysql, MYSQL_OPT_RECONNECT, (void *)"1"); //set auto reconnect.
+        std::string host = _config.get<std::string>("SantiagoDBTables.host");
+        std::string user = _config.get<std::string>("SantiagoDBTables.user");
+        std::string password = _config.get<std::string>("SantiagoDBTables.password");
+        std::string db = _config.get<std::string>("SantiagoDBTables.db");
+
         if(mysql_real_connect(_mysql,
-                              config_.get<const char*>("SantiagoDBTables.host"),
-                              config_.get<const char*>("SantiagoDBTables.user"),
-                              config_.get<const char*>("SantiagoDBTables.password"),
-                              config_.get<const char*>("SantiagoDBTables.db"),
-                              config_.get<unsigned>("SantiagoDBTables.port"),
+                              host.c_str(),
+                              user.c_str(),
+                              password.c_str(),
+                              db.c_str(),
+                              _config.get<unsigned>("SantiagoDBTables.port"),
                               NULL,
                               0) == NULL)
         {
-            ST_LOG_ERROR("mysql_real_connect() failed. host = "<<config_.get<const char*>("SantiagoDBTables.host")
-                         <<" user = "<<config_.get<const char*>("SantiagoDBTables.user")
-                         <<" db = "<<config_.get<const char*>("SantiagoDBTables.db")
-                         <<" port = "<<config_.get<unsigned>("SantiagoDBTables.port")<<std::endl);
+            ST_LOG_ERROR("mysql_real_connect() failed. host = " << config_.get<const char*>("SantiagoDBTables.host")
+                         <<" user = " << config_.get<const char*>("SantiagoDBTables.user")
+                         <<" db = " << config_.get<const char*>("SantiagoDBTables.db")
+                         <<" port = " << config_.get<unsigned>("SantiagoDBTables.port") << std::endl);
 
             error_ = std::error_code(ERR_DATABASE_EXCEPTION, ErrorCategory::GetInstance());
         }
         
-        ST_LOG_INFO("mysql_real_connect() succeeded." <<config_.get<const char*>("SantiagoDBTables.host")
-                    <<" user = "<<config_.get<const char*>("SantiagoDBTables.user")
-                    <<" db = "<<config_.get<const char*>("SantiagoDBTables.db")<<std::endl);
+        ST_LOG_INFO("mysql_real_connect() succeeded." << config_.get<const char*>("SantiagoDBTables.host")
+                    <<" user = " << config_.get<const char*>("SantiagoDBTables.user")
+                    <<" db = " << config_.get<const char*>("SantiagoDBTables.db") << std::endl);
        
-        error = std::error_code(ERR_SUCCESS, ErrorCategory::GetInstance());
+        error_ = std::error_code(ERR_SUCCESS, ErrorCategory::GetInstance());
     }
 
     void MariaDBConnection::disconnect(std::error_code& error_)
     {
-        ST_LOG_INFO("Closing db connection..."<<std::endl);
+        ST_LOG_INFO("Closing db connection..." << std::endl);
         mysql_close(_mysql);
-        error = std::error_code(ERR_SUCCESS, ErrorCategory::GetInstance());
+        error_ = std::error_code(ERR_SUCCESS, ErrorCategory::GetInstance());
     }
 
-    void MariaDBConnection::runQueryImpl(const std::string& queryString_,std::error_code& error_)
+    void MariaDBConnection::runQueryImpl(const std::string& queryString_, std::error_code& error_)
     {
-        ST_LOG_INFO("Running query:"<<std::endl
-                    <<queryString_<<std::endl);
+        ST_LOG_INFO("Running query:" << std::endl
+                    << queryString_ << std::endl);
 
         if(mysql_query(_mysql, queryString_.c_str()) ||
            (0 != mysql_errno(_mysql)))
         {
-            ST_LOG_DEBUG("Db error:"<< mysql_error(_mysql)<<std::endl);
+            ST_LOG_DEBUG("Db error:"<< mysql_error(_mysql) << std::endl);
             error_ = std::error_code(ERR_DATABASE_EXCEPTION, ErrorCategory::GetInstance());
         }
 
-        error = std::error_code(ERR_SUCCESS, ErrorCategory::GetInstance());
+        error_ = std::error_code(ERR_SUCCESS, ErrorCategory::GetInstance());
     }
 
-    int MariaDBConnection::runInsertQuery(const std::string& queryString_,std::error_code& error_)
+    int MariaDBConnection::runInsertQuery(const std::string& queryString_, std::error_code& error_)
     {    
-        ST_LOG_INFO("Running insert query:");
-        runQueryImpl(queryString_,error_);
+        ST_LOG_INFO("Running insert query:" << std::endl);
+        runQueryImpl(queryString_, error_);
         if(error_)
         {
             return INVALID_DATABASE_ID;
@@ -84,24 +88,24 @@ namespace Santiago{ namespace SantiagoDBTables
 
         if(0 == mysql_affected_rows(_mysql))
         {
-            ST_LOG_DEBUG("mysql_affected_rows() returns 0.")
+            ST_LOG_DEBUG("mysql_affected_rows() returns 0.");
             error_ = std::error_code(ERR_DATABASE_QUERY_FAILED, ErrorCategory::GetInstance());
             return INVALID_DATABASE_ID;
         }
 
         int ret = mysql_insert_id(_mysql);
-        error = std::error_code(ERR_SUCCESS, ErrorCategory::GetInstance());
-        ST_LOG_INFO("Insert query successful."<<std::endl);
+        error_ = std::error_code(ERR_SUCCESS, ErrorCategory::GetInstance());
+        ST_LOG_INFO("Insert query successful." << std::endl);
 
         return ret;
     }
 
     void MariaDBConnection::runSelectQuery(const std::string& queryString_,
-                                           const std::function<void(std::error_code&)>& postQueryFn_,
+                                           const std::function<void(MYSQL_RES *, std::error_code&)>& postQueryFn_,
                                            std::error_code& error_)
     {
-        ST_LOG_INFO("Running select query:");
-        runQueryImpl(queryString_,error_);
+        ST_LOG_INFO("Running select query:" << std::endl);
+        runQueryImpl(queryString_, error_);
         if(error_)
         {
             return;
@@ -110,29 +114,29 @@ namespace Santiago{ namespace SantiagoDBTables
         MYSQL_RES *result = mysql_store_result(_mysql);
         if(result == NULL)
         {
-            ST_LOG_DEBUG("mysql_store_result() returned null."<<std::endl);
+            ST_LOG_DEBUG("mysql_store_result() returned null." << std::endl);
             error_ = std::error_code(ERR_DATABASE_EXCEPTION, ErrorCategory::GetInstance());
             return;
         }
 
         if(0 == mysql_num_rows(result))
         {
-            ST_LOG_DEBUG("mysql_num_rows() returned 0."<<std::endl);
+            ST_LOG_DEBUG("mysql_num_rows() returned 0." << std::endl);
             error_ = std::error_code(ERR_DATABASE_QUERY_FAILED, ErrorCategory::GetInstance());
             mysql_free_result(result);
             return;
         }
         
-        postQueryFn_(result,error_);
+        postQueryFn_(result, error_);
         mysql_free_result(result);
-        ST_LOG_INFO("Select query successful."<<std::endl);
+        ST_LOG_INFO("Select query successful." << std::endl);
         return;
     }
 
-    void MariaDBConnection::runUpdateQuery(const std::string& queryString_,std::error_code& error_)
+    void MariaDBConnection::runUpdateQuery(const std::string& queryString_, std::error_code& error_)
     {
-        ST_LOG_INFO("Running update query:");
-        runQueryImpl(queryString_,error_);
+        ST_LOG_INFO("Running update query:" << std::endl);
+        runQueryImpl(queryString_, error_);
         if(error_)
         {
             return;
@@ -140,20 +144,20 @@ namespace Santiago{ namespace SantiagoDBTables
 
         if(0 == mysql_affected_rows(_mysql))
         {
-            ST_LOG_DEBUG("mysql_affected_rows() returns 0.");
+            ST_LOG_DEBUG("mysql_affected_rows() returns 0." << std::endl);
             error_ = std::error_code(ERR_DATABASE_QUERY_FAILED, ErrorCategory::GetInstance());
-            return INVALID_DATABASE_ID;
+            return;
         }
 
-        error = std::error_code(ERR_SUCCESS, ErrorCategory::GetInstance());
-        ST_LOG_INFO("Update query successful."<<std::endl);
+        error_ = std::error_code(ERR_SUCCESS, ErrorCategory::GetInstance());
+        ST_LOG_INFO("Update query successful." << std::endl);
         return;
     }
 
-    void MariaDBConnection::runDeleteQuery(const std::string& queryString_,std::error_code& error_)
+    void MariaDBConnection::runDeleteQuery(const std::string& queryString_, std::error_code& error_)
     {
-        ST_LOG_INFO("Running delete query:");
-        runQueryImpl(queryString_,error_);
+        ST_LOG_INFO("Running delete query:" << std::endl);
+        runQueryImpl(queryString_, error_);
         if(error_)
         {
             return;
@@ -161,57 +165,58 @@ namespace Santiago{ namespace SantiagoDBTables
 
         if(0 == mysql_affected_rows(_mysql))
         {
-            ST_LOG_DEBUG("mysql_affected_rows() returns 0.");
+            ST_LOG_DEBUG("mysql_affected_rows() returns 0." << std::endl);
             error_ = std::error_code(ERR_DATABASE_QUERY_FAILED, ErrorCategory::GetInstance());
-            return INVALID_DATABASE_ID;
+            return;
         }
 
-        error = std::error_code(ERR_SUCCESS, ErrorCategory::GetInstance());
-        ST_LOG_INFO("Delete query successful."<<std::endl);
+        error_ = std::error_code(ERR_SUCCESS, ErrorCategory::GetInstance());
+        ST_LOG_INFO("Delete query successful." << std::endl);
         return;
     }
 
-    void MariaDBConnection::addUserProfilesRec(UserProfilesRec& userProfilesRec_,std::error_code& error_)
+    void MariaDBConnection::addUserProfilesRec(UserProfilesRec& userProfilesRec_, std::error_code& error_)
     {
         std::string addUserProfilesRecQuery = "INSERT INTO user_profiles(user_name,password) VALUES('" +
             userProfilesRec_._userName + "', '" + userProfilesRec_._password + "')";
 
-        userProfilesRec_._id = runInsertQuery(addUserProfilesRecQuery,error_);
+        userProfilesRec_._id = runInsertQuery(addUserProfilesRecQuery, error_);
     }
 
-    boost::optional<UserProfilesRec> getUserProfilesRec(const std::string& userName_,std::error_code& error_)
+    boost::optional<UserProfilesRec> MariaDBConnection::getUserProfilesRec(const std::string& userName_,
+                                                                           std::error_code& error_)
     {
         std::string getUserProfilesRecQuery = "SELECT * FROM user_profiles WHERE username = '" + userName_ + "'";
         boost::optional<UserProfilesRec> userProfilesRec;
         runSelectQuery(
             getUserProfilesRecQuery,
-            [&ret](MYSQL_RES* mysqlResult_, std::error_code& error_)
+            [&userProfilesRec](MYSQL_RES* mysqlResult_, std::error_code& error_)
             {
                 if(mysql_num_rows(mysqlResult_) > 1)
                 {
                     ST_LOG_DEBUG("More than 1 records with same username in the user_profiles table"
-                              <<std::endl);
+                                 << std::endl);
                     error_ = std::error_code(ERR_DATABASE_EXCEPTION, ErrorCategory::GetInstance());
                     BOOST_ASSERT(false);
                 }
                 
-                MYSQL_ROW row = mysql_fetch_row(result);
+                MYSQL_ROW row = mysql_fetch_row(mysqlResult_);
                 BOOST_ASSERT(NULL != row);
                 
                 userProfilesRec->_id = atoi(row[0]);
                 userProfilesRec->_userName = row[1];
                 userProfilesRec->_password = row[2];
-                error = std::error_code(ERR_SUCCESS, ErrorCategory::GetInstance());
-            }
+                error_ = std::error_code(ERR_SUCCESS, ErrorCategory::GetInstance());
+            },
             error_);
 
-        return ret;
+        return userProfilesRec;
     }
 
-    void MariaDBConnection::updateUserProfilesRec(UserProfilesRec& newUserProfilesRec_,std::error_code& error_)
+    void MariaDBConnection::updateUserProfilesRec(UserProfilesRec& newUserProfilesRec_, std::error_code& error_)
     {
         std::string updateUserProfilesRecQuery = "UPDATE user_profiles SET password ='" +
-            userProfilesRec_._password + "' WHERE username = '" + userProfilesRec_._userName +"'";
+            newUserProfilesRec_._password + "' WHERE username = '" + newUserProfilesRec_._userName +"'";
         runUpdateQuery(updateUserProfilesRecQuery,error_);
     }
 
@@ -219,20 +224,20 @@ namespace Santiago{ namespace SantiagoDBTables
     {
         std::string deleteUserProfilesRecQuery = "DELETE FROM user_profiles WHERE username = '" +
                     userName_ + "'";
-        runUpdateQuery(updateUserProfilesRecQuery,error_);
+        runDeleteQuery(deleteUserProfilesRecQuery,error_);
     }
 
-    void MariaDBConnection::addSessionsRec(SessionsRec& sessionsRec_,std::error_code& error_)
+    void MariaDBConnection::addSessionsRec(SessionsRec& sessionsRec_, std::error_code& error_)
     {
-        std::string addSessionsRecQuery = "INSERT INTO sessions(username,cookie_string,login_time,logout_time, last_active_time) values('" +
+        std::string addSessionsRecQuery =
+            "INSERT INTO sessions(username, cookie_string, login_time, logout_time, last_active_time) values('" +
             sessionsRec_._userName + "', '" +
-            sessionsRec_._cookieId + "', '" +
-            Utils::ConvertPTimeToString(sessionsRec._loginTime) + "'," +
-            sessionsRec._logoutTime? "'" + Utils::ConvertPTimeToString(sessionsRec._logoutTime) + "'": "NULL" + ", '" +
-            Utils::ConvertPTimeToString(sessionsRec._lastActiveTime) +
-            "')";
-
-        sessionsRec_._id = runInsertQuery(addUserProfilesRecQuery,error_);
+            sessionsRec_._cookieString + "', '" +
+            Utils::ConvertPtimeToString(sessionsRec_._loginTime) + "', '" +
+            (sessionsRec_._logoutTime? Utils::ConvertPtimeToString(*(sessionsRec_._logoutTime)) : "NULL") + "', '" +
+            Utils::ConvertPtimeToString(sessionsRec_._lastActiveTime) + "')";
+            
+        sessionsRec_._id = runInsertQuery(addSessionsRecQuery,error_);
     }
 
     boost::optional<SessionsRec> MariaDBConnection::getSessionsRec(const std::string& cookieString_,
@@ -243,42 +248,42 @@ namespace Santiago{ namespace SantiagoDBTables
 
         runSelectQuery(
             getSessionsRecQuery,
-            [&ret](MYSQL_RES* mysqlResult_, std::error_code& error_)
+            [&sessionsRec](MYSQL_RES* mysqlResult_, std::error_code& error_)
             {
                 if(mysql_num_rows(mysqlResult_) > 1)
                 {
                     ST_LOG_DEBUG("More than 1 records with same cookie_string in the sessions table "
-                              <<std::endl);
+                                 << std::endl);
                     error_ = std::error_code(ERR_DATABASE_EXCEPTION, ErrorCategory::GetInstance());
                     BOOST_ASSERT(false);
                 }
                 
-                MYSQL_ROW row = mysql_fetch_row(result);
+                MYSQL_ROW row = mysql_fetch_row(mysqlResult_);
                 BOOST_ASSERT(NULL != row);
 
                 sessionsRec->_id = atoi(row[0]);
                 sessionsRec->_userName = row[1];
                 sessionsRec->_cookieString = row[2];
-                sessionsRec->_loginTime = Utils::ConvertStringToPTime(row[3]);
+                sessionsRec->_loginTime = Utils::ConvertStringToPtime(row[3]);
 
                 if(NULL != row[4])
                 {
-                    sessionsRec->_logoutTime = Utils::ConvertStringToPTime(row[4]);
+                    sessionsRec->_logoutTime = Utils::ConvertStringToPtime(row[4]);
                 }
 
-                sessionsRec->_lastActiveTime = Utils::ConvertStringToPTime(row[5]);
+                sessionsRec->_lastActiveTime = Utils::ConvertStringToPtime(row[5]);
 
-                error = std::error_code(ERR_SUCCESS, ErrorCategory::GetInstance());
-            }
+                error_ = std::error_code(ERR_SUCCESS, ErrorCategory::GetInstance());
+            },
             error_);
 
         return sessionsRec;
     }
-    void MariaDBConnection::updateSessionsRec(SessionsRec& userProfilesRec_,std::error_code& error_)
+    void MariaDBConnection::updateSessionsRec(SessionsRec& sessionsRec_, std::error_code& error_)
     {
-        std::string updateSessionsRecQuery = "UPDATE sessions SET logout_time = " +
-            userProfilesRec_._logoutTime? ("'" + Utils::ConvertPTimeToString(userProfilesRec_._logoutTime) + "', "): "NULL, " + 
-            "last_update_time = '" + Utils::ConvertPTimeToString(userProfilesRec_._lastUpdateTime) +
+        std::string updateSessionsRecQuery = "UPDATE sessions SET logout_time = '" +
+            (sessionsRec_._logoutTime? Utils::ConvertPtimeToString(*(sessionsRec_._logoutTime)) : "NULL") + "', '" + 
+            "last_update_time = '" + Utils::ConvertPtimeToString(sessionsRec_._lastActiveTime) +
             "' WHERE cookie_string ='" +
             sessionsRec_._cookieString + "'";
         runUpdateQuery(updateSessionsRecQuery,error_);
