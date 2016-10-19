d70f3d70c0ef6802e05ca30047d8c66d6b7d7f24
Vijayaraj <vijayaraj@kefassoftwares.com>
2016-10-19 11:20:04 +0530

    Cleared error in PTimeUtils

--- src/Santiago/Utils/PTimeUtils.cpp
+++ src/Santiago/Utils/PTimeUtils.cpp
@@ -5,12 +5,12 @@
 
 namespace Santiago{ namespace Utils
 {
-    boost::posix_time::ptime ConvertStringToPTime(const std::string& timeStr_)
+    boost::posix_time::ptime ConvertStringToPtime(const std::string& timeStr_)
     {
         return boost::posix_time::time_from_string(timeStr_);
     }
 
-    std::string ConvertPTimeToString(const boost::posix_time::ptime& ptime_)
+    std::string ConvertPtimeToString(const boost::posix_time::ptime& ptime_)
     {
         std::string timeStr = boost::gregorian::to_iso_extended_string(ptime_.date()) + ' ' +
             boost::posix_time::to_simple_string(ptime_.time_of_day());
