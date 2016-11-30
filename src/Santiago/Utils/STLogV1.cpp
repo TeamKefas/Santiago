bdd20f72962235507d8bb164a1c0052c5bb3fc6d
wilson <wilson@kefassoftwares.com>
2016-11-30 15:08:27 +0530

    Added new Log classes.

---
+++ src/Santiago/Utils/STLogV1.cpp
@@ -0,0 +1,76 @@
+#include <fstream>
+
+#include <boost/assert.hpp>
+
+#include "STLogV1.h"
+
+namespace Santiago{ namespace Utils
+{
+    STLog& STLog::GetInstance()
+    {
+        static STLog instance;
+        return instance;
+    }
+
+    STLog::STLog():
+        _fileNameStreamPair(boost::none),
+        _mutex()
+    {}
+
+    void STLog::setOutputFile(const std::string& fileName_)
+    {
+        if(_fileNameStreamPair && (fileName_ == _fileNameStreamPair->first))
+        {
+            return;
+        }
+        initFile(fileName_);
+    }
+
+    std::ostream& STLog::stream()
+    {
+        if(_fileNameStreamPair)
+        {
+            return *_fileNameStreamPair->second;
+        }
+        else
+        {
+            initFile("STLog_" +
+                     boost::posix_time::to_iso_string(boost::posix_time::microsec_clock::local_time()) +
+                     ".txt");
+            return *_fileNameStreamPair->second;
+        }
+    }
+
+    void STLog::initFile(const std::string& fileName_)
+    {
+        _fileNameStreamPair.reset(std::make_pair(fileName_,
+                                                 OStreamPtr(new std::ofstream(fileName_,
+                                                                              std::ios_base::out|std::ios_base::app))));
+        
+        if(!std::static_pointer_cast<std::ofstream>(_fileNameStreamPair->second)->is_open())
+        {
+//            std::cerr<<"Failed to open log file"<<std::endl;
+            throw std::runtime_error("Failed to open log file.");
+        }
+    }
+
+}}
+
+
+namespace boost
+{
+    void assertion_failed(char const * expr, char const * function, char const * file, long line)
+    {
+        ST_LOG_CRITICAL("Asserted in File:" << file << " LineNo:" << line << " Function:" << function << " Expr:" 
+            << expr);
+        assert(false);
+    }
+
+    void assertion_failed_msg(char const * expr, char const * msg, char const * function, char const * file, long line)
+    {
+        ST_LOG_CRITICAL("Asserted in File:" << file << " LineNo:" << line << " Function:" << function << " Expr:" 
+                        << expr << " Msg:" << msg);
+        assert(false);
+    }
+
+} // closing namespace boost
---
+++ src/Santiago/Utils/STLogV1.h
@@ -0,0 +1,61 @@
+#ifndef SANTIAGO_UTILS_STLOG_H
+#define SANTIAGO_UTILS_STLOG_H
+
+#include <string>
+#include <ostream>
+#include <memory>
+#include <mutex>
+#include <thread>
+
+#define BOOST_ENABLE_ASSERT_HANDLER 1
+
+#include <boost/assert.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/optional.hpp>
+
+namespace Santiago{ namespace Utils
+{
+    class STLog
+    {
+    public:
+
+        static STLog& GetInstance();
+
+        std::ostream& stream();
+        std::mutex& mutex() {return _mutex;}
+
+        void setOutputFile(const std::string& fileName_);
+
+        ~STLog();
+
+    protected:
+
+        STLog();
+        void initFile(const std::string& fileName_);
+
+        typedef std::shared_ptr<std::ostream> OStreamPtr;
+
+        boost::optional<std::pair<std::string,OStreamPtr> >  _fileNameStreamPair;
+        std::mutex                                           _mutex;
+    };
+
+}} //closing namespace Santiago::Utils
+
+#define SOURCE_FILE (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : strrchr(__FILE__, '\\') ? \
+strrchr(__FILE__, '\\') + 1 : __FILE__)
+
+#define ST_LOG_IMPL(statement,log_type)\
+    {\
+        boost::posix_time::ptime now = boost::posix_time::microsec_clock::local_time();\
+        std::lock_guard<std::mutex> guard(Santiago::Utils::STLog::GetInstance().mutex());\
+        Santiago::Utils::STLog::GetInstance().stream()\
+            << SOURCE_FILE << "(LineNo:" << __LINE__ << ") ThreadNo:"<<std::this_thread::get_id() << " ["\
+            << boost::posix_time::to_simple_string(now)<<"] "<< log_type << statement << std::endl; \
+    }\
+
+#define ST_LOG_DEBUG(statement) ST_LOG_IMPL(statement,"DEBUG")
+#define ST_LOG_INFO(statement) ST_LOG_IMPL(statement,"INFO")
+#define ST_LOG_ERROR(statement) ST_LOG_IMPL(statement,"ERROR")
+#define ST_LOG_CRITICAL(statement) ST_LOG_IMPL(statement,"CRITICAL")
+
+#endif
