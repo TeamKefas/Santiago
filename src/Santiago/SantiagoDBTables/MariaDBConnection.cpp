cf937108a83f21d60424ebf9fdf88e335216284b
Vijayaraj <vijayaraj@kefassoftwares.com>
2017-01-18 16:32:49 +0530

    Enumerated fields

--- src/Santiago/SantiagoDBTables/MariaDBConnection.cpp
+++ src/Santiago/SantiagoDBTables/MariaDBConnection.cpp
@@ -12,7 +12,7 @@ namespace Santiago{ namespace SantiagoDBTables
         std::error_code error;
         connect(error);
     }
-    
+
     MariaDBConnection::~MariaDBConnection()
     {
         std::error_code error;
@@ -41,18 +41,18 @@ namespace Santiago{ namespace SantiagoDBTables
                               0) == NULL)
         {
             ST_LOG_ERROR("mysql_real_connect() failed. host = " 
-                         << _config.get<std::string>("Santiago.SantiagoDBTables.host")
-                         <<" user = " << _config.get<std::string>("Santiago.SantiagoDBTables.user")
-                         <<" db = " << _config.get<std::string>("Santiago.SantiagoDBTables.db")
-                         <<" port = " << _config.get<std::string>("Santiago.SantiagoDBTables.port") << std::endl);
+                         << config_.get<const char*>("Santiago.SantiagoDBTables.host")
+                         <<" user = " << config_.get<const char*>("Santiago.SantiagoDBTables.user")
+                         <<" db = " << config_.get<const char*>("Santiago.SantiagoDBTables.db")
+                         <<" port = " << config_.get<unsigned>("Santiago.SantiagoDBTables.port") << std::endl);
 
             error_ = std::error_code(ERR_DATABASE_EXCEPTION, ErrorCategory::GetInstance());
 	    BOOST_ASSERT(false);
         }
         
-        ST_LOG_INFO("mysql_real_connect() succeeded." << _config.get<std::string>("Santiago.SantiagoDBTables.host")
-                    <<" user = " << _config.get<std::string>("Santiago.SantiagoDBTables.user")
-                    <<" db = " << _config.get<std::string>("Santiago.SantiagoDBTables.db") << std::endl);
+        ST_LOG_INFO("mysql_real_connect() succeeded." << config_.get<const char*>("Santiago.SantiagoDBTables.host")
+                    <<" user = " << config_.get<const char*>("Santiago.SantiagoDBTables.user")
+                    <<" db = " << config_.get<const char*>("Santiago.SantiagoDBTables.db") << std::endl);
        
         error_ = std::error_code(ERR_SUCCESS, ErrorCategory::GetInstance());
     }
@@ -225,10 +225,14 @@ namespace Santiago{ namespace SantiagoDBTables
             return boost::none;
         }
 
-        std::string getUsersRecQuery = "SELECT * FROM ST_users WHERE user_name = '" + userName_ + "'";
+        std::string getUsersRecQuery = "SELECT id, "
+            "user_name, "
+            "email_address, "
+            "password "
+            "FROM ST_users WHERE user_name = '" + userName_ + "'";
         return getUsersRecImpl(getUsersRecQuery,error_); 
     }
-    
+
     boost::optional<UsersRec> MariaDBConnection::getUsersRecForEmailAddress(
         const std::string& emailAddress_,
         std::error_code& error_)
@@ -239,7 +243,11 @@ namespace Santiago{ namespace SantiagoDBTables
             return boost::none;
         }
 
-        std::string getUsersRecQuery = "SELECT * FROM ST_users WHERE email_address = '" + emailAddress_ + "'";
+        std::string getUsersRecQuery = "SELECT id, "
+            "user_name, "
+            "email_address, "
+            "password "
+            "FROM ST_users WHERE email_address = '" + emailAddress_ + "'";
         return getUsersRecImpl(getUsersRecQuery,error_); 
     }
 
@@ -341,7 +349,13 @@ namespace Santiago{ namespace SantiagoDBTables
             return boost::none;
         }
 
-        std::string getSessionsRecQuery = "SELECT * FROM ST_sessions WHERE cookie_string = '" + cookieString_ + "'";
+        std::string getSessionsRecQuery = "SELECT id, "
+            "user_name, "
+            "cookie_string, "
+            "login_time, "
+            "logout_time, "
+            "last_active_time "
+            "FROM ST_sessions WHERE cookie_string = '" + cookieString_ + "'";
         boost::optional<SessionsRec> sessionsRec;
 
         runSelectQuery(
@@ -406,7 +420,13 @@ namespace Santiago{ namespace SantiagoDBTables
     std::vector<SessionsRec> MariaDBConnection::getActiveSessions(std::error_code& error_)
     {
         // ptime logoutTime;//(from_iso_string("00000000T000000"));
-        std::string getActiveSessionsQuery =  "SELECT * FROM ST_sessions WHERE logout_time IS NULL";
+        std::string getActiveSessionsQuery =  "SELECT id, "
+            "user_name, "
+            "cookie_string, "
+            "login_time, "
+            "logout_time, "
+            "last_active_time "
+            "FROM ST_sessions WHERE logout_time IS NULL";
         std::vector<SessionsRec> sessionsRecs;
         runSelectQuery(
             getActiveSessionsQuery,
