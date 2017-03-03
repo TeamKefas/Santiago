5cad17519bd0bc6475a9d85c1a47d3fda5bbb3b5
Vijayaraj <vijayaraj@kefassoftwares.com>
2017-03-03 11:44:06 +0530

    Added password hashing function in RequestHandlerBase

--- src/Santiago/User/ServerV1/RequestHandlerBase.h
+++ src/Santiago/User/ServerV1/RequestHandlerBase.h
@@ -16,7 +16,6 @@
 #include "../../ThreadSpecificVar/ThreadSpecificVar.h"
 
 #include "ServerMessage.h"
-//#include "DatabaseInterface.h"
 #include "../../SantiagoDBTables/MariaDBConnection.h"
 #include "ServerData.h"
 #include "ConnectionServer.h"
@@ -61,20 +60,7 @@ namespace Santiago{ namespace User { namespace Server
 
     protected:
 
-        std::string generateSHA256(const std::string str)
-        {
-            unsigned char hash[SHA256_DIGEST_LENGTH];
-            SHA256_CTX sha256;
-            SHA256_Init(&sha256);
-            SHA256_Update(&sha256, str.c_str(), str.size());
-            SHA256_Final(hash, &sha256);
-            std::stringstream ss;
-            for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
-            {
-                ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
-            }
-            return ss.str();
-        }
+        std::string generateSHA256(const std::string str);
       
         ServerData                    &_serverData;       
         SantiagoDBConnection          &_databaseConnection;
