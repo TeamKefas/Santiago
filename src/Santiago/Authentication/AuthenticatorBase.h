5f42f3cb73d105d89d1843f67c88e65c4f192434
Vijayaraj <vijayaraj@kefassoftwares.com>
2017-05-25 15:07:38 +0530

    Impl functions added in AuthenticatorBase

--- src/Santiago/Authentication/AuthenticatorBase.h
+++ src/Santiago/Authentication/AuthenticatorBase.h
@@ -186,6 +186,106 @@ namespace Santiago{ namespace Authentication
         virtual void deleteUser(const std::string& cookieString_,
                                 const ErrorCodeCallbackFn& onDeleteUserCallbackFn_) = 0;
 
+        //implementation functions
+        /**
+         * Implementation function for createUser.
+         * @param userName_ - username of the user.
+         * @param emailAddress_ - Email address of the user.
+         * @param password_ - Password received from the user.
+         * @param onCreateUserCallbackFn_ - Call back function for createUser operations
+         */
+        virtual void createUserImpl(const std::string& userName_,
+                                    const std::string& emailAddress_,
+                                    const std::string& password_,
+                                    const ErrorCodeCallbackFn& onCreateUserCallbackFn_) = 0;
+        /**
+         * Implementation function for the loginUser function.
+         * @param userNameOrEmailAddress_ - username used by the user for login.
+         * @param isUserNameNotEmailAddress_ - true for username and false for email address.
+         * @param password_ - Password used by the user for login.
+         * @param onCreateUserCallbackFn_ - Call back function for login operations
+         */
+        virtual void loginUserImpl(const std::string& userNameOrEmailAddress_,
+                                   bool isUserNameNotEmailAddress_,
+                                   const std::string& password_,
+                                   const ErrorCodeUserInfoStringPairCallbackFn& onLoginUserCallbackFn_) = 0;
+        /**
+         * Implementation function for the verifyCookieAndGetUserInfo function.
+         * @param cookieString_ - cookieString of the user that is used to get user information.
+         * @param onVerifyUserCallbackFn_ - Call back function for verify cookie and get user info operations.
+         */
+        virtual void verifyCookieAndGetUserInfoImpl(const std::string& cookieString_,
+                                                    const ErrorCodeUserInfoCallbackFn& onVerifyUserCallbackFn_) = 0;
+        /**
+         * This function is the implementation function for the logoutUserForCookie function.
+         * @param cookieString_ - cookieString of the user that is used for logout operation.
+         * @param onLogoutCookieCallbackFn_ - Call back function for logout user for cookie operations
+         */
+        virtual void logoutUserForCookieImpl(const std::string& cookieString_,
+                                             const ErrorCodeCallbackFn& onLogoutCookieCallbackFn_) = 0;
+        /**
+         * This function is the implementation function for the logoutUserForAllCookies function.
+         * @param userName_ - username of the user.
+         * @param onLogoutAllCookiesCallbackFn_ - Call back function for logout for all cookie operations.
+         */
+        virtual void logoutUserForAllCookiesImpl(const std::string& userName_,
+                                                 const ErrorCodeCallbackFn& onLogoutAllCookiesCallbackFn_) = 0;
+        /**
+         * This function is the implementation function for the changeUserPassword function.
+         * @param cookieString_ - Cookie from the user.
+         * @param oldPassword_ - Old password of the user.
+         * @param newPassword_ - New password of the user.
+         * @param onChangePasswordCallbackFn_ - Call back function for password change operations.
+         */
+        virtual void changeUserPasswordImpl(const std::string& cookieString_,
+                                            const std::string& oldPassword_,
+                                            const std::string& newPassword_,
+                                            const ErrorCodeCallbackFn& onChangePasswordCallbackFn_) = 0;
+        /**
+         * This function is the implementation function for the getUserForEmailAddressAndRecoveryString.
+         * @param emailAddress_ - Email address  received from the user.
+         * @param onGetUserForEmailAddressAndRecoveryStringCallbackFn_ -Call back function for get user for email address operations.
+         */
+        virtual void getUserForEmailAddressAndRecoveryStringImpl(const std::string& emailAddress_,
+                                                                 const std::string& recoveryString_,
+                                                                 const ErrorCodeStringCallbackFn& onGetUserForEmailAddressAndRecoveryStringCallbackFn_) = 0;
+        /**
+         * This function is the implementation function for the changeUserPasswordForEmailAddressAndRecoveryString function.
+         * @param emailAddress_ - Email address from the user.
+         * @param recoveryString_ - Recovery string from the user.
+         * @param newPassword_ - New password of the user.
+         * @param onChangePasswordForEmailAddressAndRecoveryStringCallbackFn_ - Call back function for password change for email address and recovery string operations.
+         */
+        virtual void changeUserPasswordForEmailAddressAndRecoveryStringImpl(const std::string& emailAddress_,
+                                                                            const std::string& recoverystring_,
+                                                                            const std::string& newPassword_,
+                                                                            const ErrorCodeCallbackFn& onChangePasswordForEmailAddressAndRecoveryStringCallbackFn_) = 0;
+        /**
+         * This function is the implementation function for the changeUserEmailAddress function. 
+         * @param cookieString_ - Cookie from the user.
+         * @param newEmailAddress_ - New email address that is to be updated.
+         * @param password_ - Password of the user account.
+         * @param onChangeEmailAddressCallbackFn_ - Call back operations for email change operations.
+         */
+        virtual void changeUserEmailAddressImpl(const std::string& cookieString_,
+                                                const std::string& newEmailAddress_,
+                                                const std::string& password_,
+                                                const ErrorCodeCallbackFn& onChangeEmailAddressCallbackFn_) = 0;
+        /**
+         *  This function is the implementation function for the createAndReturnRecoveryString function.
+         * @param emailAddress_ - Email Address from the user.
+         * @param onCreateAndReturnRecoveryStringCallbackFn_ - Call back function for create ang return recovery string operations.
+         */
+        virtual void createAndReturnRecoveryStringImpl(const std::string& emailAddress_,
+                                                       const ErrorCodeStringCallbackFn& onCreateAndReturnRecoveryStringCallbackFn_) = 0;
+        /**
+         *  This function is the implementation function for the deleteUser function.
+         * @param cookieString_ - Cookie from the user.
+         * @param onDeleteUserCallbackFn_ - Call back function for delete user operations.
+         */
+        virtual void deleteUserImpl(const std::string& cookieString_,
+                                    const ErrorCodeCallbackFn& onDeleteUserCallbackFn_) = 0;
+
     protected:
         
         boost::asio::io_service         &_ioService;
