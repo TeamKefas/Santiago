1e876ac4c63a3df40044c3834d218e2c88190b69
junais <junu743@gmail.com>
2017-06-06 17:55:54 +0530

    mysql connect error

--- CMakeModules/FindMariaDB.cmake
+++ CMakeModules/FindMariaDB.cmake
@@ -1,32 +1,4 @@
 #
-# DEFINES
-# ------
-# MARIADB_INCLUDE_DIR - MariaDB include directory
-# MARIADB_LIBRARIES - Libraries required to link MariaDB
-# MARIADB_FOUND - Confirmation
-
-find_path(MARIADB_INCLUDE_DIR mysql.h	
-          /usr/include/mysql
-          /usr/local/include/mysql
-          )
-
-find_library(MARIADB_LIBRARIES NAMES mysqlclient
-            PATHS
-            /usr/lib/mysql
-            /usr/local/lib/mysql
-            /opt/mysql/lib/mysql
-            )
-
-if(MARIADB_INCLUDE_DIR AND MARIADB_LIBRARIES)
-    set(MARIADB_FOUND TRUE)
-    message(STATUS "Found MariaDB: ${MARIADB_INCLUDE_DIR}, ${MARIADB_LIBRARIES}")
-  else(MARIADB_INCLUDE_DIR AND MARIADB_LIBRARIES)
-set(MARIADB_FOUND FALSE)
-    message(STATUS "MariaDB not found.")
-endif(MARIADB_INCLUDE_DIR AND MARIADB_LIBRARIES)
-
-mark_as_advanced(MARIADB_INCLUDE_DIR MARIADB_LIBRARIES)
-
 # FindMariaDB.cmake
 #
 # Try to find the include directory
@@ -64,7 +36,7 @@ IF(WIN32)
   ENDIF()
 
 ELSE()
-  SET(MARIADB_LIB libmysqlclient.a)
+  SET(MARIADB_LIB libmariadbclient.a)
   FIND_PATH(MARIADB_BIN_DIR mariadb_config
     $ENV{MARIADB_DIR}/bin
     ${MARIADB_DIR}/bin)
@@ -87,4 +59,3 @@ IF(MARIADB_LIBRARY_DIR AND MARIADB_INCLUDE_DIR)
   MESSAGE(STATUS "Found MariaDB libraries: ${MARIADB_LIBRARY_DIR}")
   SET(MARIADB_FOUND TRUE)
 ENDIF()
-
