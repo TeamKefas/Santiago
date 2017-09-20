763d7eab13ac99a7239b64eba7c6788fafcb1800
Vijayaraj <vijayaraj@kefassoftwares.com>
2017-09-20 18:22:51 +0530

    Updated Authenticator in SingleNode.

---
+++ src/Santiago/Authentication/SingleNode/AuthenticatorV1.cpp
@@ -0,0 +1,23 @@
+#include <cctype>
+
+#include "AuthenticatorV1.h"
+
+namespace Santiago{ namespace Authentication{ namespace SingleNode
+{
+    typedef std::shared_ptr<boost::asio::strand> StrandPtr;
+    typedef std::shared_ptr<AuthenticatorImplBase> AuthenticatorImplBasePtr;
+    
+    std::pair<AuthenticatorImplBasePtr,StrandPtr> Authenticator::getAuthenticatorAndStrandForString(
+        const std::string& string_,
+        bool isNotEmailAddress_)
+    {
+        std::string userName = string_;
+        std::error_code error;
+        if(!isNotEmailAddress_)
+        {
+            boost::optional<SantiagoDBTables::UsersRec> usersRec = _databaseConnection.get().getUsersRecForEmailAddress(string_,error);
+            userName = usersRec->_userName;
+        }
+        return _authenticatorStrandPair[static_cast<int>(toupper(userName.at(0)))-65];
+    }
+}}}
--- src/Santiago/Authentication/SingleNode/AuthenticatorV1.h
+++ src/Santiago/Authentication/SingleNode/AuthenticatorV1.h
@@ -1,24 +1,38 @@
 #ifndef SANTIAGO_AUTHENTICATION_SINGLENODE_AUTHENTICATORV1_H
 #define SANTIAGO_AUTHENTICATION_SINGLENODE_AUTHENTICATORV1_H
 
-#include "Santiago/Authentication/AuthenticatorBase.h"
+#include "AuthenticatorImpl.h"
+#include "Santiago/Authentication/AuthenticatorBaseV1.h"
 #include "Santiago/Thread/ThreadSpecificVar.h"
+#include "Santiago/SantiagoDBTables/MariaDBConnection.h"
 
 namespace Santiago{ namespace Authentication{ namespace SingleNode
 {
-    //TODO: Update AuthenticatorBase before implementing this
     class Authenticator:public AuthenticatorBase
     {
     public:
         typedef Thread::ThreadSpecificVar<SantiagoDBTables::MariaDBConnection> ThreadSpecificDbConnection;
-        
+
         Authenticator(boost::asio::io_service& ioService_,
                       const boost::property_tree::ptree& config_,
-                      ThreadSpecificDbConnection& databaseConnection_)
-
-        virtual std::pair<ControllerPtr,StrandPtr> getControllerAndStrandForString(
+                      const ClientIdType& clientId_,
+                      ThreadSpecificDbConnection& databaseConnection_):AuthenticatorBase(ioService_,config_,clientId_),_databaseConnection(databaseConnection_)
+        {
+            for(int i = 0; i < 26; ++i)
+            {
+                auto strandPtr = std::make_shared<boost::asio::strand>(ioService_);
+                auto authenticatorImplBasePtr = std::make_shared<AuthenticatorImpl>(_databaseConnection);
+                _authenticatorStrandPair[i] = std::make_pair(authenticatorImplBasePtr,strandPtr);
+            }
+        }
+                
+        virtual std::pair<AuthenticatorImplBasePtr,StrandPtr> getAuthenticatorAndStrandForString(
             const std::string& string_,
             bool isNotEmailAddress_);
+
+    private: 
+        ThreadSpecificDbConnection& _databaseConnection;       
+        std::pair<AuthenticatorImplBasePtr,StrandPtr> _authenticatorStrandPair[26];
     };
 
 }}}
