36dd9b9cc65ff0c656defbf886da322bc5357949
Vijayaraj <vijayaraj@kefassoftwares.com>
2017-11-29 15:41:21 +0530

    Adding RequestHandlers

---
+++ src/Santiago/Authentication/MultiNode/Server/DeleteUserRequestHandler.cpp
@@ -0,0 +1,44 @@
+#include "DeleteUserRequestHandler.h"
+
+
+namespace Santiago{namespace Authentication { namespace Server
+{
+    DeleteUserRequestHandler::DeleteUserRequestHandler(ConnectionServer& connectionServer_,
+                                                       ServerData& serverData_,
+                                                       const OnCompletedCallbackFn& onCompletedCallbackFn_,
+                                                       const ConnectionMessage& initiatingMessage_)
+        :RequestHandlerBase(connectionServer_,serverData_,onCompletedCallbackFn_,initiatingMessage_)
+    {}
+    
+    void DeleteUserRequestHandler::handleRequest()
+    {
+        std::string cookieString = _initiatingMessage._parameters[0];
+        std::string userName;
+        boost::optional<SantiagoDBTables::UsersRec> sessionsRec = serverData_._databaseConnection.get().getSessionsRec(cookieString,error);
+        if(sessionsRec)
+        {
+            userName = sessionsRec._userName;
+        }
+        std::pair<ControllerPtr,StrandPtr> authenticatorStrandPair =
+            _serverData._authenticatorStrandPair[static_cast<int>(toupper(userName[0]))
+                                                 - static_cast<int>('a')];
+        
+        boost::asio::spawn(
+            *authenticatorStrandPair.second,
+            [authenticatorStrandPair,cookieString](boost::asio::yield_context yield_)
+            {
+                std::error_code error;
+                error = authenticatorStrandPair.first->deleteUser(cookieString,yield_);
+                ConnectionMessage replyMessage(_initiatingMessage._requestId,
+                                               ConnectionMessageType::SUCEEDED,
+                                               std::vector<std::string>());
+                if(error)
+                {
+                    replyMessage._type = ConnectionMessageType::FAILED;
+                }
+                _connectionServer.sendMessage(replyMessage,false,boost::none);
+                _onCompletedCallbackFn(_initiatingMessage._requestId);
+            });
+    }
+
+}}}
---
+++ src/Santiago/Authentication/MultiNode/Server/DeleteUserRequestHandler.h
@@ -0,0 +1,38 @@
+#ifndef SANTIAGO_AUTHENTICATION_SERVER_DELETEUSERREQUESTHANDLER_H
+#define SANTIAGO_AUTHENTICATION_SERVER_DELETEUSERREQUESTHANDLER_H
+
+/**
+ * @file DeleteUserRequestHandler.h
+ *
+ * @section DESCRIPTION
+ *
+ * Contains the DeleteUserRequestHandler class  
+ */
+
+#include "RequestHandlerBase.h"
+
+namespace Santiago{namespace Authentication { namespace Server
+{
+
+    class DeleteUserRequestHandler:public RequestHandlerBase
+    {
+    public:
+        /**
+         * The constructor
+         * @param connectionServer_- 
+         * @param onCompletedCallbackFn_- ///NEED TO WRITE\\\
+         * @param initiatingMessage_ -
+         */
+        DeleteUserRequestHandler(ConnectionServer& connectionServer_,
+                                 ServerData& serverData_,
+                                 const OnCompletedCallbackFn& onCompletedCallbackFn_,
+                                 const ConnectionMessage& initiatingMessage_);
+       /**
+        * ///Message\\
+        */
+        virtual void handleRequest();
+    };
+
+}}}
+
+#endif
