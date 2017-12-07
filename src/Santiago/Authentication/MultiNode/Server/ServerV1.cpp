f2109833361a4e62954caf2d36a7a76951c6a834
Vijayaraj <vijayaraj@kefassoftwares.com>
2017-12-07 13:37:00 +0530

    Cleaning up

--- src/Santiago/Authentication/MultiNode/Server/ServerV1.cpp
+++ src/Santiago/Authentication/MultiNode/Server/ServerV1.cpp
@@ -43,66 +43,101 @@ namespace Santiago{ namespace User { namespace MultiNode { namespace Server
             
         case ConnectionMessageType::CR_CREATE_USER:
             requestHandlerPtr.reset(new CreateUserRequestHandler
-                                    (_connectionServer
-                                     ,std::bind(&Server::handleRequestCompleted
-                                                ,this,std::placeholders::_1)
-                                     ,message_));
+                                    (_connectionServer,
+                                     _serverData,
+                                     std::bind(&Server::handleRequestCompleted
+                                               ,this,std::placeholders::_1),
+                                     message_));
             break;
         case ConnectionMessageType::CR_LOGIN_USER:
             requestHandlerPtr.reset(new LoginUserRequestHandler
-                                    (_connectionServer
-                                     ,std::bind(&Server::handleRequestCompleted
-                                                ,this,std::placeholders::_1)
-                                     ,message_));
+                                    (_connectionServer,
+                                     std::bind(&Server::handleRequestCompleted
+                                                ,this,std::placeholders::_1),
+                                     message_));
             break;
             
-        case ConnectionMessageType::CR_VERIFY_USER_FOR_COOKIE:
+        case ConnectionMessageType::CR_VERIFY_COOKIE_AND_GET_USER_INFO:
             requestHandlerPtr.reset(new VerifyUserForCookieRequestHandler
-                                    (_connectionServer
-                                     ,std::bind(&Server::handleRequestCompleted
-                                                ,this,std::placeholders::_1)
-                                     ,message_));
+                                    (_connectionServer,
+                                     _serverData,
+                                     std::bind(&Server::handleRequestCompleted
+                                               ,this,std::placeholders::_1),
+                                     message_));
             break;
             
         case ConnectionMessageType::CR_LOGOUT_USER_FOR_COOKIE:
             requestHandlerPtr.reset(new LogoutUserForCookieRequestHandler
-                                    (_connectionServer
-                                     ,std::bind(&Server::handleRequestCompleted
-                                                ,this,std::placeholders::_1)
-                                     ,message_));
+                                    (_connectionServer,
+                                     _serverData,
+                                     std::bind(&Server::handleRequestCompleted
+                                               ,this,std::placeholders::_1),
+                                     message_));
             break;
             
         case ConnectionMessageType::CR_LOGOUT_USER_FOR_ALL_COOKIES:
             requestHandlerPtr.reset(new LogoutUserForAllCookiesRequestHandler
-                                    (_connectionServer
-                                     ,std::bind(&Server::handleRequestCompleted
-                                                ,this,std::placeholders::_1)
-                                     ,message_));
+                                    (_connectionServer,
+                                     _serverData,
+                                     std::bind(&Server::handleRequestCompleted
+                                               ,this,std::placeholders::_1),
+                                     message_));
             break;
             
         case  ConnectionMessageType::CR_CHANGE_USER_PASSWORD:
             requestHandlerPtr.reset(new ChangeUserPasswordRequestHandler
-                                    (_connectionServer
-                                     ,std::bind(&Server::handleRequestCompleted
-                                                ,this,std::placeholders::_1)
-                                     ,message_));
+                                    (_connectionServer,
+                                     _serverData,
+                                     std::bind(&Server::handleRequestCompleted
+                                               ,this,std::placeholders::_1),
+                                     message_));
             break;
-        case ConnectionMessageType::CR_REMOVED_COOKIE_FROM_APPSERVER:
-            requestHandlerPtr.reset(new RemovedCookieFromAppserverRequestHandler
-                                    (_connectionServer
-                                     ,std::bind(&Server::handleRequestCompleted
-                                                ,this,std::placeholders::_1)
-                                     ,message_));
+
+            case  ConnectionMessageType::CR_GET_USER_FOR_EMAIL_ADDRESS_AND_RECOVERY_STRING:
+            requestHandlerPtr.reset(new GetUserForEmailAddressAndRecoveryStringRequestHandler
+                                    (_connectionServer,
+                                     _serverData,
+                                     std::bind(&Server::handleRequestCompleted
+                                               ,this,std::placeholders::_1),
+                                     message_));
             break;
-            
-        case ConnectionMessageType::CR_ADD_RESOURCE:
-            requestHandlerPtr.reset(new AddResourceRequestHandler
-                                    (_connectionServer
-                                     ,std::bind(&Server::handleRequestCompleted
-                                                ,this,std::placeholders::_1)
-                                     ,message_));
+
+            case  ConnectionMessageType::CR_CHANGE_USER_PASSWORD_FOR_EMAIL_ADDRESS_AND_RECOVERY_STRING:
+            requestHandlerPtr.reset(new ChangeUserPasswordForEmailAddressAndRecoveryStringRequestHandler
+                                    (_connectionServer,
+                                     _serverData,
+                                     std::bind(&Server::handleRequestCompleted
+                                                ,this,std::placeholders::_1),
+                                     message_));
             break;
-             
+
+            case  ConnectionMessageType::CR_CHANGE_USER_EMAIL_ADDRESS:
+            requestHandlerPtr.reset(new ChangeUserEmailAddressRequestHandler
+                                    (_connectionServer,
+                                     _serverData,
+                                     std::bind(&Server::handleRequestCompleted
+                                                ,this,std::placeholders::_1),
+                                     message_));
+            break;
+
+            case  ConnectionMessageType::CR_CREATE_AND_RETURN_RECOVERY_STRING:
+            requestHandlerPtr.reset(new CreateAndReturnRecoveryStringRequestHandler
+                                    (_connectionServer,
+                                     _serverData,
+                                     std::bind(&Server::handleRequestCompleted
+                                                ,this,std::placeholders::_1),
+                                     message_));
+            break;
+
+            case  ConnectionMessageType::CR_DELETE_USER:
+            requestHandlerPtr.reset(new DeleteUserRequestHandler
+                                    (_connectionServer,
+                                     _serverData,
+                                     std::bind(&Server::handleRequestCompleted
+                                                ,this,std::placeholders::_1),
+                                     message_));
+            break;
+                   
         case ConnectionMessageType::SR_LOGOUT_USER_FOR_COOKIE:
             BOOST_ASSERT(false);
             break;
