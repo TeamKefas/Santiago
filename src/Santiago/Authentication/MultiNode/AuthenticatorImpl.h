e13a0ad94695bdc3c5a3af0c81020eb879736c2c
Wilson Philip <wilson@kefassoftwares.com>
2018-01-01 13:03:42 +0530

    Implemented model verifyCookieAndGetUserInfo fn in MultiNode AuthenticatorImpl.

--- src/Santiago/Authentication/MultiNode/AuthenticatorImpl.h
+++ src/Santiago/Authentication/MultiNode/AuthenticatorImpl.h
@@ -39,90 +39,94 @@ namespace Santiago{ namespace Authentication{ namespace MultiNode
 
     protected:
         
-        virtual void createUserImpl(const std::string& userName_,
-                                    const std::string& emailAddress_,
-                                    const std::string& password_,
-                                    const ErrorCodeCallbackFn& onCreateUserCallbackFn_);
+        void createUserImpl(const std::string& userName_,
+                            const std::string& emailAddress_,
+                            const std::string& password_,
+                            const ErrorCodeCallbackFn& onCreateUserCallbackFn_);
         
         void handleCreateUserConnectionMessage(const std::error_code& error_,
                                                const boost::optional<ConnectionMessage>& connectionMessageOpt_,
                                                const ErrorCodeCallbackFn& onCreateUserCallbackFn_);
         
-        virtual void loginUserImpl(const std::string& userNameOrEmailAddress_,
-                                   bool isUserNameNotEmailAddress_,
-                                   const std::string& password_,
-                                   const ErrorCodeUserInfoStringPairCallbackFn& onLoginUserCallbackFn_);
+        void loginUserImpl(const std::string& userNameOrEmailAddress_,
+                           bool isUserNameNotEmailAddress_,
+                           const std::string& password_,
+                           const ErrorCodeUserInfoStringPairCallbackFn& onLoginUserCallbackFn_);
         
         void handleLoginUserConnectionMessage(const std::error_code& error_,
                                               const boost::optional<ConnectionMessage>& connectionMessageOpt_,
                                               const ErrorCodeUserInfoStringPairCallbackFn& onLoginUserCallbackFn_);
 
-        virtual void verifyCookieAndGetUserInfoImpl(const std::string& cookieString_,
-                                                    const ErrorCodeUserInfoCallbackFn& onVerifyUserCallbackFn_);
+        std::pair<std::error_code,boost::optional<UserInfo> >
+        verifyCookieAndGetUserInfo(const std::string& cookieString_,
+                                   boost::asio::yield_context yield_)
+                    
+        void verifyCookieAndGetUserInfoImpl(const std::string& cookieString_,
+                                            const ErrorCodeUserInfoCallbackFn& onVerifyUserCallbackFn_);
         
         void handleVerifyCookieConnectionMessage(const std::error_code& error_,
                                                  const boost::optional<ConnectionMessage>& connectionMessageOpt_,
                                                  const ErrorCodeUserInfoCallbackFn& onVerifyUserCallbackFn_);
 
-        virtual void logoutUserForCookieImpl(const std::string& cookieString_,
-                                             const ErrorCodeCallbackFn& onLogoutCookieCallbackFn_);
+        void logoutUserForCookieImpl(const std::string& cookieString_,
+                                     const ErrorCodeCallbackFn& onLogoutCookieCallbackFn_);
 
         void handleLogoutUserForCookieConnectionMessage(const std::error_code& error_,
                                                         const boost::optional<ConnectionMessage>& connectionMessageOpt_,
                                                         const ErrorCodeCallbackFn& onLogoutCookieCallbackFn_);
 
-        virtual void logoutUserForAllCookiesImpl(const std::string& userName_,
-                                                 const ErrorCodeCallbackFn& onLogoutAllCookiesCallbackFn_);
+        void logoutUserForAllCookiesImpl(const std::string& userName_,
+                                         const ErrorCodeCallbackFn& onLogoutAllCookiesCallbackFn_);
 
         void handleLogoutUserForAllCookiesConnectionMessage(const std::error_code& error_,
                                                             const boost::optional<ConnectionMessage>& connectionMessageOpt_,
                                                             const ErrorCodeCallbackFn& onLogoutAllCookiesCallbackFn_);
 
-        virtual void changeUserPasswordImpl(const std::string& cookieString_,
-                                            const std::string& oldPassword_,
-                                            const std::string& newPassword_,
-                                            const ErrorCodeCallbackFn& onChangePasswordCallbackFn_);
+        void changeUserPasswordImpl(const std::string& cookieString_,
+                                    const std::string& oldPassword_,
+                                    const std::string& newPassword_,
+                                    const ErrorCodeCallbackFn& onChangePasswordCallbackFn_);
 
         void handleChangeUserPasswordConnectionMessage(const std::error_code& error_,
                                                        const boost::optional<ConnectionMessage>& connectionMessageOpt_,
                                                        const ErrorCodeCallbackFn& onChangePasswordCallbackFn_);
 
-        virtual void getUserForEmailAddressAndRecoveryStringImpl(const std::string& emailAddress_,
-                                                                 const std::string& recoveryString_,
-                                                                 const ErrorCodeStringCallbackFn& onGetUserForEmailAddressAndRecoveryStringCallbackFn_);
+        void getUserForEmailAddressAndRecoveryStringImpl(const std::string& emailAddress_,
+                                                         const std::string& recoveryString_,
+                                                         const ErrorCodeStringCallbackFn& onGetUserForEmailAddressAndRecoveryStringCallbackFn_);
 
         void handleGetUserForEmailAddressAndRecoveryStringConnectionMessage(const std::error_code& error_,
                                                                             const boost::optional<ConnectionMessage>& connectionMessageOpt_,
                                                                             const ErrorCodeStringCallbackFn& onGetUserForEmailAddressAndRecoveryStringCallbackFn_);
 
-        virtual void changeUserPasswordForEmailAddressAndRecoveryStringImpl(const std::string& emailAddress_,
-                                                                            const std::string& recoverystring_,
-                                                                            const std::string& newPassword_,
-                                                                            const ErrorCodeCallbackFn& onChangePasswordForEmailAddressAndRecoveryStringCallbackFn_);
+        void changeUserPasswordForEmailAddressAndRecoveryStringImpl(const std::string& emailAddress_,
+                                                                    const std::string& recoverystring_,
+                                                                    const std::string& newPassword_,
+                                                                    const ErrorCodeCallbackFn& onChangePasswordForEmailAddressAndRecoveryStringCallbackFn_);
 
         void handleChangeUserPasswordForEmailAddressAndRecoveryStringConnectionMessage(const std::error_code& error_,
                                                                                        const boost::optional<ConnectionMessage>& connectionMessageOpt_,
                                                                                        const ErrorCodeCallbackFn& onChangePasswordForEmailAddressAndRecoveryStringCallbackFn_);
 
-        virtual void changeUserEmailAddressImpl(const std::string& cookieString_,
-                                                const std::string& newEmailAddress_,
-                                                const std::string& password_,
-                                                const ErrorCodeCallbackFn& onChangeEmailAddressCallbackFn_);
+        void changeUserEmailAddressImpl(const std::string& cookieString_,
+                                        const std::string& newEmailAddress_,
+                                        const std::string& password_,
+                                        const ErrorCodeCallbackFn& onChangeEmailAddressCallbackFn_);
 
         void handleChangeUserEmailAddressConnectionMessage(const std::error_code& error_,
                                                            const boost::optional<ConnectionMessage>& connectionMessageOpt_,
                                                            const ErrorCodeCallbackFn& onChangeEmailAddressCallbackFn_);
         
-        virtual void createAndReturnRecoveryStringImpl(const std::string& emailAddress_,
-                                                       const ErrorCodeStringCallbackFn& onCreateAndReturnRecoveryStringCallbackFn_);
+        void createAndReturnRecoveryStringImpl(const std::string& emailAddress_,
+                                               const ErrorCodeStringCallbackFn& onCreateAndReturnRecoveryStringCallbackFn_);
 
         void handleCreateAndReturnRecoveryStringConnectionMessage(const std::error_code& error_,
                                                                   const boost::optional<ConnectionMessage>& connectionMessageOpt_,
                                                                   const ErrorCodeStringCallbackFn& onCreateAndReturnRecoveryStringCallbackFn_);
 
-        virtual void deleteUserImpl(const std::string& cookieString_,
-                                    const ErrorCodeCallbackFn& onDeleteUserCallbackFn_);
-
+        void deleteUserImpl(const std::string& cookieString_,
+                            const ErrorCodeCallbackFn& onDeleteUserCallbackFn_);
+        
         void handleDeleteUserConnectionMessage(const std::error_code& error_,
                                                const boost::optional<ConnectionMessage>& connectionMessageOpt_,
                                                const ErrorCodeCallbackFn& onDeleteUserCallbackFn_);
